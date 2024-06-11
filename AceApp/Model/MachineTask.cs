696bd2c2b9eac5322c1ce587712ddd03c22a36ec
RavenGoku <raven.goku@gmail.com>
2024-06-11 15:20:28 +0100

    Create Model classes

---
+++ AceApp/Model/MachineTask.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceApp.Model
+{
+    public class MachineTask
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Indexed]
+        public int MachineId { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsTaskComplete { get; set; }
+
+        public string FileLocation { get; set; }
+    }
+}
---
+++ AceApp/Model/TaskDescription.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AceApp.Model
+{
+    public class TaskDescription
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Indexed]
+        public int TaskId { get; set; }
+
+        public Image TaskImage { get; set; }
+        public string Description { get; set; }
+    }
+}
