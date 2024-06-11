357063857945bae50836119f8e3fa782862366ae
RavenGoku <raven.goku@gmail.com>
2024-06-11 15:10:49 +0100

    Add project files.

---
+++ AceApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AceApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
